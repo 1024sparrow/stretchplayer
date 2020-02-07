dcf82f9070d4e5c930dffdb79debf0dc8a788e27
1024sparrow <1024sparrow@gmail.com>
2020-02-07 06:37:33 +0300

    realized function "5"(request playback position) ; tab-indents instead of space-indents

--- src/main.cpp
+++ src/main.cpp
@@ -33,133 +33,136 @@
 
 int main(int argc, char* argv[])
 {
-    StretchPlayer::Configuration config(argc, argv);
+	StretchPlayer::Configuration config(argc, argv);
 
-    if(config.help() || ( !config.ok() )) {
+	if(config.help() || ( !config.ok() )) {
 	config.usage();
 	if( !config.ok() ) return -1;
 	return 0;
-    }
+	}
 
-    if( !config.quiet() ) {
+	if( !config.quiet() ) {
 	config.copyright();
-    }
+	}
 
-    std::unique_ptr<StretchPlayer::EngineMessageCallback> _engine_callback;
-    std::unique_ptr<StretchPlayer::Engine> _engine(new StretchPlayer::Engine(&config));
+	std::unique_ptr<StretchPlayer::EngineMessageCallback> _engine_callback;
+	std::unique_ptr<StretchPlayer::Engine> _engine(new StretchPlayer::Engine(&config));
 
-    printf("enter a command (enter \"h\" for help).\n");
-    char c;
-    ssize_t dataLen;
-    char str[1024];
-    char *paramString;
-    while (true)
-    {
-        dataLen = read(0, &str, 1024);
-        if (dataLen <= 0)
-        {
-            printf("file IO error\n");
-            break;
-        }
-        if (dataLen == 1)
-            continue; // no command
-        c = str[0];
-        paramString = str + 1;
-        str[dataLen - 1] = '\0';
-        if (c == 'q')
-            break;
-        else if (c == 'h')
-        {
-            printf("########################################\n");
-            printf("# Команды от пользователя:\n");
-            printf("#  q - выйти.\n");
-            printf("#  h - отобразить справку по консольным командам управления.\n");
-            printf("#  1 - открыть. Сразу после этой команды необходимо ввести путь к файлу.\n");
-            printf("#  2 - начать воспроизведение. Параметр: милисекунда начала\n");
-            printf("#  3 - Начать воспроизведение. Парметры: милисекунды начала и конца воспроизведения.\n");
-            printf("#  4 - Прекратить воспроизведение. Возвращает милисекунду останова.\n");
-            printf("#  5 - запрос позиции воспроизведения. Возвращает милисекунду текущего воспроизведения.\n");
-            printf("#  6 - задать скорость воспроизведения (в процентах).\n");
-            printf("#  7 - задать смещение частот.\n");
-            printf("#  8 - установить громкость (в процентах)");
-            printf("#\n");
-            printf("# Сообщения пользователю:\n");
-            printf("#  0 - сообщение об ошибке (текст).\n");
-            printf("#  1 - успешность открытия. Без параметров.\n");
-            printf("#  4 - милисекунда останова.\n");
-            printf("#  6 - скорость воспроизведения. Посылается в ответ на команды 2, 3 и 6.\n");
-            printf("#  7 - смещение частот (число от -12 до 12). ПОсылается вответ на команды 2, 3 и 7.\n");
-            // установить громкость
-            //
-            //
-            //
-            printf("########################################\n");
-        }
-        else if (c == '1')
-        {
-            _engine->load_song(paramString);
-        }
-        else if (c == '2')
-        {
-            long long ll = atoll(paramString);
-            double d = ll/1000.;
-            printf("%f\n", d);
-            _engine->locate(d);
-            _engine->play();
-        }
-        else if (c == '3')
-        {
-            long long ll1, ll2;
-            const char *s = strtok(paramString, " ");
-            if (s)
-                ll1 = atoll(s);
-            else
-            {
-                printf("error");
-                continue;
-            }
-            s = strtok(NULL, " ");
-            if (s)
-                ll2 = atoll(s);
-            else
-            {
-                printf("error");
-                continue;
-            }
-            printf("%lli - %lli\n", ll1, ll2);
-            double d1 = ll1/1000.;
-            _engine->locate(d1);
-            _engine->play();
-        }
-        else if (c == '4')
-        {
-            _engine->stop();
-        }
-        else if (c == '5')
-        {
-        }
-        else if (c == '6')
-        {
-            short i = atoi(paramString);
-            float d = i/100.;
-            _engine->set_stretch(d);
-        }
-        else if (c == '7')
-        {
-            short i = atoi(paramString);
-            _engine->set_pitch(i);
-        }
-        else if (c == '8')
-        {
-            short i = atoi(paramString);
-            float d = i / 100.;
-            _engine->set_volume(d);
-        }
-        else
-        {
-            printf("=============: %c\n", c);
-            printf("else: \"%s\"\n", paramString);
-        }
-    }
-    return 0;
+	printf("enter a command (enter \"h\" for help).\n");
+	char c;
+	ssize_t dataLen;
+	char str[1024];
+	char *paramString;
+	while (true)
+	{
+		dataLen = read(0, &str, 1024);
+		if (dataLen <= 0)
+		{
+			printf("file IO error\n");
+			break;
+		}
+		if (dataLen == 1)
+			continue; // no command
+		c = str[0];
+		paramString = str + 1;
+		str[dataLen - 1] = '\0';
+		if (c == 'q')
+			break;
+		else if (c == 'h')
+		{
+			printf("########################################\n");
+			printf("# Команды от пользователя:\n");
+			printf("#  q - выйти.\n");
+			printf("#  h - отобразить справку по консольным командам управления.\n");
+			printf("#  1 - открыть. Сразу после этой команды необходимо ввести путь к файлу.\n");
+			printf("#  2 - начать воспроизведение. Параметр: милисекунда начала\n");
+			printf("#  3 - Начать воспроизведение. Парметры: милисекунды начала и конца воспроизведения.\n");
+			printf("#  4 - Прекратить воспроизведение. Возвращает милисекунду останова.\n");
+			printf("#  5 - запрос позиции воспроизведения. Возвращает милисекунду текущего воспроизведения.\n");
+			printf("#  6 - задать скорость воспроизведения (в процентах).\n");
+			printf("#  7 - задать смещение частот.\n");
+			printf("#  8 - установить громкость (в процентах)");
+			printf("#\n");
+			printf("# Сообщения пользователю:\n");
+			printf("#  0 - сообщение об ошибке (текст).\n");
+			printf("#  1 - успешность открытия. Без параметров.\n");
+			printf("#  4 - милисекунда останова.\n");
+			printf("#  5 - милисекунда воспроизведения.\n");
+			printf("#  6 - скорость воспроизведения. Посылается в ответ на команды 2, 3 и 6.\n");
+			printf("#  7 - смещение частот (число от -12 до 12). ПОсылается вответ на команды 2, 3 и 7.\n");
+			// установить громкость
+			//
+			//
+			//
+			printf("########################################\n");
+		}
+		else if (c == '1')
+		{
+			_engine->load_song(paramString);
+		}
+		else if (c == '2')
+		{
+			long long ll = atoll(paramString);
+			double d = ll/1000.;
+			//printf("%f\n", d);
+			_engine->locate(d);
+			_engine->play();
+		}
+		else if (c == '3')
+		{
+			long long ll1, ll2;
+			const char *s = strtok(paramString, " ");
+			if (s)
+				ll1 = atoll(s);
+			else
+			{
+				printf("error");
+				continue;
+			}
+			s = strtok(NULL, " ");
+			if (s)
+				ll2 = atoll(s);
+			else
+			{
+				printf("error");
+				continue;
+			}
+			printf("%lli - %lli\n", ll1, ll2);
+			double d1 = ll1/1000.;
+			_engine->locate(d1);
+			_engine->play();
+		}
+		else if (c == '4')
+		{
+			_engine->stop();
+		}
+		else if (c == '5')
+		{
+			float position = 1000. * _engine->get_position();
+			printf("5%f\n", position);
+		}
+		else if (c == '6')
+		{
+			short i = atoi(paramString);
+			float d = i/100.;
+			_engine->set_stretch(d);
+		}
+		else if (c == '7')
+		{
+			short i = atoi(paramString);
+			_engine->set_pitch(i);
+		}
+		else if (c == '8')
+		{
+			short i = atoi(paramString);
+			float d = i / 100.;
+			_engine->set_volume(d);
+		}
+		else
+		{
+			printf("=============: %c\n", c);
+			printf("else: \"%s\"\n", paramString);
+		}
+	}
+	return 0;
 }
